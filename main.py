d541b61fcc2e6bb3f285d79aaff24a6c736c24b7
ahmadgaz <105146133+ahmadgaz@users.noreply.github.com>
2023-05-03 12:33:56 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+  print(client.get_channel(os.getenv("CHANNEL_ID")))
+  channel = client.get_channel(os.getenv("CHANNEL_ID"))
+
+  magnet_state = sys.argv[1];
+
+#   await channel.send(f"The club room is now {magnet_state}")
+
+client.run(os.getenv("TOKEN"))
